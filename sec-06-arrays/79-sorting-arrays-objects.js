919dbc6148b8e3fc29c1966d81d4e2316caccc2b
Jack Gamboa <jgamboa929@gmail.com>
2019-02-19 10:44:21 -0600

    Working with arrays.

---
+++ sec-06-arrays/79-sorting-arrays-objects.js
@@ -0,0 +1,12 @@
+// Sorting arrays with objects
+const courses = [{ id: 1, name: "Node.js" }, { id: 2, name: "javaScript" }];
+
+courses.sort((a, b) => {
+  const nameA = a.name.toUpperCase();
+  const nameB = b.name.toUpperCase();
+  if (nameA < nameB) return -1;
+  if (nameA > nameB) return 1;
+  return 0;
+});
+
+console.log(courses);
---
+++ sec-06-arrays/79-sorting-arrays-primitives.js
@@ -0,0 +1,8 @@
+// Sorting primitives
+const numbers = [2, 3, 1];
+numbers.sort();
+
+console.log(numbers);
+
+numbers.reverse();
+console.log(numbers);
